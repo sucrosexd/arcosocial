381f6a2f94b821d67c83503d255dede5ddb4d46d
nagisus <97454944+sucrosexd@users.noreply.github.com>
2025-03-29 08:48:56 +0400

    фикс нужной хрени лол

--- scripts/posts.js
+++ scripts/posts.js
@@ -10,7 +10,7 @@ const posts = {
         comments: []
     },
     3: {
-        title: "",
+        title: "EGTS 2025 приближается к концу",
         likes: 10,
         comments: []
     }
@@ -33,10 +33,10 @@ function toggleLike(postId) {
     localStorage.setItem(`post_${postId}_likes`, posts[postId].likes);
 }
 function postComment(postId) {
-    const comment = document.querySelector('textarea').value;
+    const comment = document.querySelectorAll('textarea').value;
     if(comment) {
         posts[postId].comments.push(comment);
-        document.querySelector('textarea').value = '';
+        document.querySelectorAll('textarea').value = '';
         updateComments(postId);
         console.log("да")
     }
